10c94b650e3e88cec0a06c3e0afbb7882745c5d8
Ivan-Tulcan <103902761+Ivan-Tulcan@users.noreply.github.com>
2023-01-12 08:55:10 -0500

    Add files via upload

---
+++ EjercicioFactoryMethod/EjercicioFactoryMethod/Fish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioFactoryMethod
+{
+    class Fish : IAnimals
+    {
+        public string Sound()
+        {
+            return "Fish Sound";
+        }
+    }
+
+}
---
+++ EjercicioFactoryMethod/EjercicioFactoryMethod/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using EjercicioFactoryMethod;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Que animal deseas escuchar?:");
+            Console.WriteLine("1. Mamifero");
+            Console.WriteLine("2. Pajaro");
+            Console.WriteLine("3. Pez");
+            Console.WriteLine("4. Ninguno");
+
+            int option = int.Parse(Console.ReadLine());
+
+            if (option == 1)
+            {
+                Creator mammalCreator = new ConcreteCreatorMammal();
+                IAnimals mammal = mammalCreator.FactoryMethod();
+                Console.WriteLine("Mammal Sound: " + mammal.Sound());
+            }
+            else if (option == 2)
+            {
+                Creator birdCreator = new ConcreteCreatorBird();
+                IAnimals bird = birdCreator.FactoryMethod();
+                Console.WriteLine("Bird Sound: " + bird.Sound());
+            }
+            else if (option == 3)
+            {
+                Creator fishCreator = new ConcreteCreatorFish();
+                IAnimals fish = fishCreator.FactoryMethod();
+                Console.WriteLine("Fish Sound: " + fish.Sound());
+            }
+            else if (option == 4)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Opcion invalida. Prueba otra vez.");
+            }
+        }
+    }
+}
+
+
